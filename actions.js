5ecbea96a155c66733223a726e2df873a19a4194
BananaMonkeyTaco <bananamonkeytaco@gmail.com>
2019-01-16 21:57:06 -0500

    animation

---
+++ actions.js
@@ -0,0 +1,15 @@
+var actions = [];
+var ranNum = 0;
+function actionList(x) {
+/*  let i = 0
+  while (actions[i] == null) {
+    i++;
+  }
+  var actions[i] = x;
+  */
+  actions[actions.length] = x
+}
+
+function wander() {
+  actionlist("wander");
+}
