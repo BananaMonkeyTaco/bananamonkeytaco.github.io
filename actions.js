90f4a86bb3d6ef17d696494da93ab90b57f854dc
BananaMonkeyTaco <bananamonkeytaco@gmail.com>
2019-01-22 12:16:52 -0500

    The game plays

--- actions.js
+++ actions.js
@@ -1,48 +1,141 @@
 var actionOrder = [];
+var actionOrderList = [];
 var actionAmount = [];
 var actionAmountCompleted = [];
 var actionOrderProgress = [];
+var mana = 10000;
+var currentAction;
+var currentCostLeft;
+var gamePaused = true;
+
+var currentCycleActionList = [];
+var currentCycleActionAmount = [];
+var currentActionPlace = -1;
+
+function start() {
+  setInterval(work, 10);
+}
+
+function gamePause() {
+  if (gamePaused == false) {
+    gamePaused = true;
+  }
+}
+
+function gamePlay() {
+  if (gamePaused == true && actionOrder.length > 0) {
+    if (currentCycleActionList.length == 0) {
+      gameNewCycle();
+    }
+    gamePaused = false;
+  }
+}
+
+function gameNewCycle() {
+  currentCycleActionList.splice(0, currentCycleActionList.length);
+  currentCycleActionAmount.splice(0, currentCycleActionAmount.length);
+  currentActionPlace = -1;
+  for (let i = 0; i < actionOrder.length; i++) {
+    currentCycleActionList.push(actionOrder[i]);
+    currentCycleActionAmount.push(actionAmount[i]);
+  }
+  initializeProgressList();
+}
+
+function work() {
+  document.getElementById("mana").innerHTML = "Mana = " + mana;
+  if (gamePaused == false) {
+    if (mana == 0) {
+      gamePaused;
+    } else {
+      mana--;
+      if (currentAction == undefined) {
+        currentAction = findNextAction();
+        originalCost = calculateActualMana(currentAction);
+        currentCostLeft = calculateActualMana(currentAction);
+      }
+      progressAction(currentActionPlace);
+      updateActionProgressList();
+    }
+  }
+}
+
 function addAction(action) {
-  actionOrder.push(action.name);
+  actionOrder.push(action);
+  actionOrderList.push(action.name);
   actionAmount.push("1");
-  console.log(action.name);
   updateActionList();
 }
+
 function removeActionFromList(actionPlace) {
   actionOrder.splice(actionPlace, 1);
+  actionOrderList.splice(actionPlace, 1);
   actionAmount.splice(actionPlace, 1);
   updateActionList();
 }
+
 function increaseActionAmount(actionPlace) {
   actionAmount[actionPlace]++;
   updateActionList();
 }
+
 function decreaseActionAmount(actionPlace) {
   if(actionAmount[actionPlace] > 0){
     actionAmount[actionPlace]--;
   }
   updateActionList();
 }
+
 function initializeProgressList() {
-  for (var i = 0; i < actionOrder.length; i++) {
+  for (let i = 0; i < actionOrderList.length; i++) {
     actionAmountCompleted[i] = 0;
     actionOrderProgress[i] = 0;
   }
 }
+
+function progressAction(action) {
+  currentCostLeft--;
+  actionOrderProgress[action] =
+  (Math.round((originalCost - currentCostLeft) / originalCost)) * 100;
+  if (currentCostLeft == 0) {
+    actionAmountCompleted[action]++;
+    currentAction = undefined;
+  }
+  return;
+}
+
+function findNextAction() {
+  for (let i = 0; i < currentCycleActionList.length; i++) {
+    if (actionAmountCompleted[i] < currentCycleActionAmount[i]) {
+      console.log("for loop" + i);
+      console.log("completed" + actionAmountCompleted[i]);
+      console.log("needed" + currentCycleActionAmount[i]);
+      currentActionPlace = i;
+      return currentCycleActionList[i];
+    }
+  }
+}
+
+function calculateActualMana(action) {
+  return action.manaCost;
+}
+
 var wander = {
   name: "Wander",
-  manaCost: 50,
+  manaCost: 500,
 };
+
 var smashPots = {
   name:  "Smash Pots",
-  manaCost: 100,
+  manaCost: 10,
 };
+
 function updateActionList() {
   var x = "";
-  for(let i = 0; i < actionOrder.length; i++) {
+  for(let i = 0; i < actionOrderList.length; i++) {
     x = x + "<div>" +
     "<span class=actionBoxActions>" +
-    actionOrder[i] + "  x" + actionAmount[i] +
+    actionOrderList[i] + "  x" + actionAmount[i] +
     "<span class=actionBoxOptions>" +
     "<span onclick=increaseActionAmount(" + i + ")>" + " + " + "</span>" +
     "<span onclick=decreaseActionAmount(" + i + ")>" + " - " + "</span>" +
@@ -51,12 +144,13 @@ function updateActionList() {
     document.getElementById('actionBoxActionList').innerHTML = x;
   }
 }
-function updateActionProgressList(){
+
+function updateActionProgressList() {
   var x = "";
-  for (let i = 0; i < actionOrder.length; i++) {
+  for (let i = 0; i < currentCycleActionList.length; i++) {
     x = x + "<div>" +
-    "<span class=actionBoxProgressList>" + actionOrder[i] +
-    "  ( " + actionAmountCompleted[i] + " / " + actionAmount[i] + " )" +
+    "<span class=actionBoxProgressList>" + currentCycleActionList[i].name +
+    "  ( " + actionAmountCompleted[i] + " / " + currentCycleActionAmount[i] + " )" +
     actionOrderProgress[i] + "%" + "</span></div>";
   }
   document.getElementById("actionBoxProgressList").innerHTML = x;
--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-var mana = 100;
-function start(){
-  setInterval(work, 10);
-}
-function work(){
-  document.getElementById("mana").innerHTML = "Mana = " + mana;
-  if (mana > 0) {
-    mana--;
-  }
-  initializeProgressList();
-  updateActionProgressList();
-}
+
